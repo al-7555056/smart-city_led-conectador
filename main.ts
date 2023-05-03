bed486ee9b267fff0bd54e4a09644adac04230a6
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2023-05-03 09:09:17 +0200

    Principios

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+input.onButtonPressed(Button.A, function () {
+    if (Control) {
+        Control = false
+        basic.showString("OFF")
+        basic.clearScreen()
+    } else {
+        basic.showString("ON")
+        Control = true
+        basic.clearScreen()
+    }
+})
+let Control = false
+Control = false
+basic.showString("Micro:bit")
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+basic.forever(function () {
+    if (Control) {
+        if (input.lightLevel() >= 128) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+        } else {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        }
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
